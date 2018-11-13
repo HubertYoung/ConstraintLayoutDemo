67a41feb32a977d6b7c2e1ce49f6825bcb53b771
Lyt <m13152870721@163.com>
2018-11-13 15:55:16 +0800

    1111

--- app/src/main/java/com/candice/constraintlayoutdemo/MyAdapter.kt
+++ app/src/main/java/com/candice/constraintlayoutdemo/MyAdapter.kt
@@ -2,6 +2,7 @@ package com.candice.constraintlayoutdemo
 
 import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,9 +17,17 @@ import android.view.ViewGroup
  * @desc:com.candice.constraintlayoutdemo
  */
 class MyAdapter(val context: Context, val dataBean: DataBean) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val TYPE_NO_TITLE: Int = 1
+    private val TYPE_CONTENT: Int = 0
+
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RecyclerView.ViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.item_layout, p0, false)
-        return MyViewHolder(view)
+        if (p1 == TYPE_NO_TITLE) {
+            val view = LayoutInflater.from(context).inflate(R.layout.item_layout2, p0, false)
+            return MyHolder(view)
+        } else {
+            val view = LayoutInflater.from(context).inflate(R.layout.item_layout, p0, false)
+            return MyViewHolder(view)
+        }
     }
 
     override fun getItemCount(): Int {
@@ -26,8 +35,20 @@ class MyAdapter(val context: Context, val dataBean: DataBean) : RecyclerView.Ada
     }
 
     override fun onBindViewHolder(p0: RecyclerView.ViewHolder, p1: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
+
+    override fun getItemViewType(position: Int): Int {
+        if (TextUtils.isEmpty(dataBean.title)) {
+            return TYPE_NO_TITLE
+        } else {
+            return TYPE_CONTENT
+        }
+    }
+}
+
+class MyHolder(view: View?) : RecyclerView.ViewHolder(view!!) {
+
 }
 
 class MyViewHolder(view: View?) : RecyclerView.ViewHolder(view!!) {
